8ce8541995b5166072785161b9f1968a9fc104a3
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-12 19:13:57 +0000

    add hover state for Tweet-Box in CSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,20 +4,28 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+//note NOT NECC!!
 //create a function that returns true when moousemove over Tweet-Box
-// function mousemove(){
-$(document).ready(function(){
-$('.Tweet-Box').on('mousemove', function(){
-    console.log(data);
-    //console.log($(this).addClass('mousehover')); 
-    //return true; 
-})
 
-});
-// }
+//$(document).ready(function(){
+// $('.Tweet-Box').on('mousemove', function(){
+//    ($(this).addClass('mousehover')); 
+// })
 
-// if (mousemove()){
-//     console.log($(this).addClass('mousehover'));
-// }
+//}); 
+//-----------------
+//option: create function -> return true? 
+//const mousemove = function (){
+// $(document).ready(function(){
+//     $('.Tweet-Box').on('mousemove', function(){
+//        return true;
+//     })
+    
+//     })
+// };
 
-//mousemove or pointermove 
+
+//$(document).ready(function(){
+//  (mousemove()) ? ($(this).addClass('mousehover')) :($(this).removeClass('mousehover')); 
+//console.log(mousemove()); 
+//}); 
