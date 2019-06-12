1841f9f6131e1c7ece25dc1ed4bc172737de905d
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-12 18:15:59 +0000

    add and styled Tweet container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,21 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+//create a function that returns true when moousemove over Tweet-Box
+// function mousemove(){
+$(document).ready(function(){
+$('.Tweet-Box').on('mousemove', function(){
+    console.log(data);
+    //console.log($(this).addClass('mousehover')); 
+    //return true; 
+})
+
+});
+// }
+
+// if (mousemove()){
+//     console.log($(this).addClass('mousehover'));
+// }
+
+//mousemove or pointermove 
