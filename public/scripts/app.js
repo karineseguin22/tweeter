96bd31e4bb967536a71ea8c31262c9a894317877
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-26 17:02:02 +0000

    Add flex-direction: column and display:flex to solve issue of margin for compose tweet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-//createTweetElement function -> takes a tweet object + return a tweet <article>
-
 
 
 $(document).ready(function(){ 
@@ -19,82 +11,60 @@ $(document).ready(function(){
     return date;
   }
 
-    // Test / driver code (temporary). Eventually will get this from the server.
-    const data = [
- 
-      ];
+  const data = [ ];
   
-    const createTweetElement = tweet => {
+  const createTweetElement = tweet => { //creates ind tweet element
         
-        let $article = $('<article>')
-        .addClass('Tweet-Box');
+    let $article = $('<article>')
+    .addClass('Tweet-Box');
 
-        let $header = $('<header>').addClass('headerheight');
+    let $header = $('<header>').addClass('headerheight');
 
-        let $topelements = $('<h2>').addClass(tweet.user.name);
+    let $topelements = $('<h2>').addClass(tweet.user.name);
 
-        let $img = $('<img>').addClass('logo').attr('src',tweet.user.avatars.small);
+    let $img = $('<img>').addClass('logo').attr('src',tweet.user.avatars.small);
 
-        let $name = $('<span>').text(tweet.user.name);
+    let $name = $('<span>').text(tweet.user.name);
 
-        let $handle = $('<h3>').addClass('aname').text(tweet.user.handle);
+    let $handle = $('<h3>').addClass('aname').text(tweet.user.handle);
 
-        let $main = $('<main>');
-''
-        let $text = $('<p>').text(tweet.content.text);
-
-        let $footer = $('<footer>').addClass('border');
-
-        let $daysposted = $('<p>').addClass('nomargin').text(getDate());
-
-        let $flag = $('<i>').addClass('fa fa-flag').addClass('hiddenIcons');
-        
-        let $retweet = $('<i>').addClass('fa fa-retweet').addClass('hiddenIcons');
-
-        let $heart = $('<i>').addClass('fa fa-heart').addClass('hiddenIcons');
+    let $main = $('<main>');
   
+    let $text = $('<p>').text(tweet.content.text);
 
-        $topelements.append($img).append($name);
-        $header.append($topelements).append($handle); 
-        $main.append($text); 
-        $footer.append($daysposted).append($flag).append($retweet).append($heart); 
-        $article.append($header).append($main).append($footer); 
+    let $footer = $('<footer>').addClass('border');
 
-        return $article;
+    let $daysposted = $('<p>').addClass('nomargin').text(getDate());
+
+    let $flag = $('<i>').addClass('fa fa-flag').addClass('hiddenIcons');
+          
+    let $retweet = $('<i>').addClass('fa fa-retweet').addClass('hiddenIcons');
+
+    let $heart = $('<i>').addClass('fa fa-heart').addClass('hiddenIcons');
+    
+
+    $topelements.append($img).append($name);
+    $header.append($topelements).append($handle); 
+    $main.append($text); 
+    $footer.append($daysposted).append($flag).append($retweet).append($heart); 
+    $article.append($header).append($main).append($footer); 
+
+    return $article;
         
     }
     
-    //create a render function called renderTweets -> takes in an ARRAY of tweet objects AND 
-//append each one to the tweets-container
+    //takes in an ARRAY of tweet objects AND append each one to the tweets-container
 function renderTweets(tweets){
-    //loop though tweets (array)
-    //var sortedTweets = tweets.sort((a,b)=> b.created_at - a.created_at)
     for (let tweet of tweets){
-    //call createTweetElement for each Tweet
     var $tweet = createTweetElement(tweet); 
         $('#Tweet-List').prepend($tweet);
-    //takes return value and appends to the tweets container
     }
 
 }
 
-
-
-  //var $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
-//$('#Tweet-List').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
 // Reusable Ajax request
 //makes a twitter post 
 const request = (options, cb) => {
-
-    /*Validate if tweet content
-    1) not too long or not present
-    2) form should not be cleared
-    3)the form should not submit 
-    */
    
     $.ajax(options)
       .done(response => cb(response))
@@ -131,11 +101,7 @@ $('#tweetform').on('submit',function(event){
 
 renderTweets(data); 
 
-
-
-//create a get request to I can see the posts when I refresh 
-
-const loadTweets = () => {
+const loadTweets = () => { //create a get request so I can see the posts when I refresh 
     const reqOptions = {
         url: '/tweets',
         method: 'get',
